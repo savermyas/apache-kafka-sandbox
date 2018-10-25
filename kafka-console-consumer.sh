7b5494828c75ae1a0faf5586774d8db56c138588
Dmitry Myasnikov <dmyasnikov@griddynamics.com>
2018-10-25 21:31:37 +0300

    Add consile producer and consumer scripts

---
+++ kafka-console-consumer.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export HOST_IP=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | head -1 | cut -d\  -f2)
+BROKERS=$(docker-compose ps | grep 9092 | awk '{print $4}' | cut -d- -f1 | sed -e "s/0.0.0.0:/$HOST_IP:/g" | tr '\n' ',')
+
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -i -t wurstmeister/kafka /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server="$BROKERS" "$@"
