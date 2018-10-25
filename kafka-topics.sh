36e78f4252d1320646b682f720b11871e71cb783
Dmitry Myasnikov <dmyasnikov@griddynamics.com>
2018-10-25 21:31:05 +0300

    Add kafka-topics script

--- kafka-topics.sh
+++ kafka-topics.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export HOST_IP=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | head -1 | cut -d\  -f2)
-export ZK="${HOST_IP}:$(docker-compose port zookeeper 2181 | cut -d: -f2)"
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$HOST_IP -e ZK=$ZK -i -t wurstmeister/kafka /opt/kafka/bin/kafka-topics.sh --zookeeper $ZK "$@"
+ZK="${HOST_IP}:$(docker-compose port zookeeper 2181 | cut -d: -f2)"
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -i -t wurstmeister/kafka /opt/kafka/bin/kafka-topics.sh --zookeeper $ZK "$@"
